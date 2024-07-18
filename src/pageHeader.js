fea7069b7325936eb76960ef85261fd6ab83b88a
Stavya1234 <stavyasharma01@gmail.com>
2024-07-18 18:59:47 +0800

    changes made to landing page related components

--- src/pageHeader.js
+++ src/pageHeader.js
@@ -6,8 +6,12 @@ function PageHeader(){
         <header>
             <ul>
                 <li className="Logo"><img src={process.env.PUBLIC_URL + "./Ascenda_Blue_Logo.jpg"} alt="Error displaying logo"></img></li>
-                <li className="SignIn">Sign in</li>
-                <li className="ContactUs">Contact Us</li>
+                <li className="SignIn">
+                    <a href="#signin">Sign in</a>
+                </li>
+                <li className="ContactUs">
+                    <a href="#contactus">Contact Us</a>
+                </li>
                 <li className="SGD"><button>SGD</button></li>
             </ul>
         
--- src/searchBar.js
+++ src/searchBar.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot, faCalendarDay, faUser } from '@fortawesome/free-solid-svg-icons';
+import 'react-datepicker/dist/react-datepicker.css';
 import './searchBar.css';
 
 function SearchBar(){
+    const [startDate, setStartDate] = useState(null);
+
     return (
         <div className="search-container">
             <div className="search-bar">
-                <input type="text" placeholder="Where to?" />
-                <input type="text" placeholder="Dates" />
-                <input type="text" placeholder="Travellers" />
-                <button>Search</button>
+                <div className="input-icon-container">
+                    <FontAwesomeIcon icon={faLocationDot} className="input-icon" />
+                    <input type="text" placeholder="Where to?" className="input-field"/>
+                </div>
+                <div className="input-icon-container">
+                    <FontAwesomeIcon icon={faCalendarDay} className="input-icon" />
+                    <DatePicker
+                        selected={startDate}
+                        onChange={(date) => setStartDate(date)}
+                        placeholderText="Dates"
+                        className="input-field date-picker-input"
+                    />
+                </div>
+                <div className="input-icon-container">
+                    <FontAwesomeIcon icon={faUser} className="input-icon" />
+                    <input type="text" placeholder="Travellers" className="input-field"/>
+                </div>
+                <button className="search-button">Search</button>
             </div>
         </div>
     );
